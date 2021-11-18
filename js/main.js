ff3d45385a56b76f1f43e964f7b37a6d103eb34f
sakiburhassan662.com <sakib9803@gmail.com>
2021-11-18 16:19:51 +0600

    changed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// bx slider
+$(document).ready(function(){
+    $('.slider-area').bxSlider();
+    $('#main-menu').meanmenu({
+      meanMenuContainer: '#mobile-menu',
+      meanScreenWidth: 768,
+    });
+  });
